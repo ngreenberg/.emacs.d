5b63059171453d925b564d446d621d0086c5df2a
Nathan Greenberg <nathanjgreenberg@gmail.com>
2016-04-22 01:55:35 -0400

    package management

---
+++ elisp/packages.el
@@ -0,0 +1,17 @@
+; list the packages you want
+(setq package-list '(solarized-theme))
+
+; list the repositories containing them
+(setq package-archives '(("melpa" . "http://melpa.org/packages/")))
+
+; activate all the packages (in particular autoloads)
+(package-initialize)
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;; (package-initialize)
+
 (add-to-list 'load-path "~/.emacs.d/elisp/")
 (load-library "style")
 (load-library "basics")
+
+(load-library "packages")
+
+(load-library "style-plus")
