1fda6b2a49b255fd79e7feface2c316ecdd7bb2a
Nathan Greenberg <nathanjgreenberg@gmail.com>
2016-12-06 16:12:51 -0500

    updated package installation

--- elisp/packages.el
+++ elisp/packages.el
@@ -1,27 +1,26 @@
+(package-initialize)
+
 ; list the packages you want
-(setq package-list '(solarized-theme
-		     smex ido-ubiquitous direx smartparens
-                     exec-path-from-shell
-		     flycheck company
-		     magit
-                     elpy py-autopep8
-		     web-mode
-                     auctex
-                     eclim))
+(custom-set-variables
+ '(package-selected-packages
+   (quote
+    (solarized-theme
+     smex ido-ubiquitous direx smartparens undo-tree expand-region
+     exec-path-from-shell
+     flycheck company
+     magit
+     elpy py-autopep8
+     web-mode
+     auctex
+     eclim))))
 
 ; list the repositories containing them
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
 
+; install the packages
+(package-install-selected-packages)
+
 ; activate all the packages (in particular autoloads)
 (package-initialize)
-
-; fetch the list of packages available
-(unless package-archive-contents
-  (package-refresh-contents))
-
-; install the missing packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
