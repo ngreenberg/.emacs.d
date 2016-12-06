3575785b0d1bfa0aa6ff160f4edb2af6187c8f0f
ngreenberg <nathanjgreenberg@gmail.com>
2016-12-06 10:26:53 -0500

    added toggle for solarized theme

--- elisp/style-plus.el
+++ elisp/style-plus.el
@@ -1,1 +1,14 @@
 (load-theme 'solarized-dark t)
+
+(defun toggle-solarized-theme ()
+  (interactive)
+  (if (equal custom-enabled-themes '(solarized-dark))
+      (progn
+	(set-frame-parameter (selected-frame) 'alpha '(100 100))
+	(disable-theme 'solarized-dark)
+	(load-theme 'solarized-light t))
+    (progn
+      (set-frame-parameter (selected-frame) 'alpha '(93 93))
+      (disable-theme 'solarized-light)
+      (load-theme 'solarized-dark t))))
+(global-set-key (kbd "C-c d") 'toggle-solarized-theme)
