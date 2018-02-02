39f89a7aefe9ab9c24d9bb78a75b555660e9c00b
ngreenberg <nathanjgreenberg@gmail.com>
2018-02-02 10:22:57 -0500

    removed gui customization

--- elisp/style.el
+++ elisp/style.el
@@ -1,15 +1,3 @@
-(defun fullscreen ()
-  (interactive)
-  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-			 '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
-(when (display-graphic-p) (fullscreen))
-
-;; set transparency
-(set-frame-parameter (selected-frame) 'alpha '(93 93))
-
-;; set font-size
-(set-face-attribute 'default (selected-frame) :height 80)
-
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
